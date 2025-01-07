c12eb0d8e7dd7df63d2d035030cf2a55674cce49
Yi Zhang <yi.zh9521@gmail.com>
2025-01-07 16:04:38 -0700

    Improvements

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,11 +5,11 @@ import { NextResponse } from 'next/server';
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
   const headersList = await headers()
   const keysArray = Array.from(headersList.keys()).sort()
-  return NextResponse.json({ message: "Server received GET request!", headers: keysArray.map(header => ({ name: header, value: headersList.get(header) })) });
+  return NextResponse.json({ message: "Server received GET request!", reqHeaders: keysArray.map(header => ({ name: header, value: headersList.get(header) })) });
 }
 
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
   const headersList = await headers()
   const keysArray = Array.from(headersList.keys()).sort()
-  return NextResponse.json({ message: "Server received POST request!", headers: keysArray.map(header => ({ name: header, value: headersList.get(header) })) });
+  return NextResponse.json({ message: "Server received POST request!", reqHeaders: keysArray.map(header => ({ name: header, value: headersList.get(header) })) });
 }
